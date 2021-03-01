be9b9e06ba5fbdd75d242bd93240c93bcbac9bbf
LuybovS <79184981+LuybovS@users.noreply.github.com>
2021-03-01 23:47:13 +0700

    Update JS_task2.js

--- JS_task2.js
+++ JS_task2.js
@@ -6,10 +6,8 @@
 // variant 1
 let arrayInput = [1,2,3,4,3,2,1];
 
-function arrayCleanPrint(arr)
-{
-  for (let index = arr.indexOf (2, 0); index !== -1; index = arr.indexOf (2, 0)) 
-  {
+function arrayCleanPrint(arr){
+  for (let index = arr.indexOf (2, 0); index !== -1; index = arr.indexOf (2, 0)){
     result = arr.splice(index, 1)
   }
   console.log(arr)
@@ -17,10 +15,8 @@ function arrayCleanPrint(arr)
 arrayCleanPrint(arrayInput);
 
 // variant 2
-function arrayCleanPrint2(arr)
-{
+function arrayCleanPrint2(arr){
   let arrayClean = arr.filter(value => { return value !== 2});
   console.log(arrayClean)
 }
-
 arrayCleanPrint2(arrayInput)
