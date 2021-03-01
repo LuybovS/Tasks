6fbd4cc34d09cddfb332fa1ea582c95bb2f9d172
LuybovS <79184981+LuybovS@users.noreply.github.com>
2021-03-01 23:37:20 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,28 @@
+// Задание №6
+// Напишите функцию, которая удаляет определенный элемент из массива.
+// (Input: arr=[1,2,3,4,3,2,1], item=2, Output: [1,3,4,3,1)
+
+
+// variant 1
+let arrayInput = [1,2,3,4,3,2,1];
+function arrayCleanPrint(arr)
+{
+  for (let index = arr.indexOf (2, 0); index !== -1; index = arr.indexOf (2, 0)) 
+  {
+    result = arr.splice(index, 1)
+  }
+  console.log(arr)
+}
+arrayCleanPrint(arrayInput);
+
+// variant 2
+
+function arrayCleanPrint2(arr)
+{
+  let arrayClean = arr.filter(value => { return value !== 2});
+  console.log(arrayClean)
+}
+
+arrayCleanPrint2(arrayInput)
+
+
